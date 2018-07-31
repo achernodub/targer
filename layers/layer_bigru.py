6104d4101826a7efe7b6f774ccc6e6d46fdc54ee
artem <a.chernodub@gmail.com>
2018-07-31 18:08:59 +0300

    Experiment

--- layers/layer_bigru.py
+++ layers/layer_bigru.py
@@ -6,23 +6,23 @@ from layers.layer_birnn_base import LayerBiRNNBase
 class LayerBiGRU(LayerBiRNNBase):
     def __init__(self, input_dim, hidden_dim, gpu):
         super(LayerBiGRU, self).__init__(input_dim, hidden_dim, gpu)
-        #self.rnn_forward_layer = nn.GRUCell(input_dim, hidden_dim)
-        #self.rnn_backward_layer = nn.GRUCell(input_dim, hidden_dim)
-        self.rnn = nn.GRU(input_size=input_dim,
-                          hidden_size=hidden_dim,
-                          num_layers=1,
-                          batch_first=True,
-                          bidirectional=True)
+        self.rnn_forward_layer = nn.GRUCell(input_dim, hidden_dim)
+        self.rnn_backward_layer = nn.GRUCell(input_dim, hidden_dim)
+        #self.rnn = nn.GRU(input_size=input_dim,
+        #                  hidden_size=hidden_dim,
+        #                  num_layers=1,
+        #                  batch_first=True,
+        #                  bidirectional=True)
         self.num_layers = 1
         self.num_directions = 2
 
-    def forward(self, input_tensor): #input_tensor shape: batch_size x max_seq_len x dim
+    def forward_new(self, input_tensor): #input_tensor shape: batch_size x max_seq_len x dim
         batch_size, max_seq_len, _ = input_tensor.shape
         h0 = self.make_gpu(torch.zeros(self.num_layers*self.num_directions, batch_size, self.hidden_dim))
         output, _ = self.rnn(input_tensor, h0)
         return output  # shape: batch_size x max_seq_len x hidden_dim*2
 
-    def forward_old(self, input_tensor): #input_tensor shape: batch_size x max_seq_len x dim
+    def forward(self, input_tensor): #input_tensor shape: batch_size x max_seq_len x dim
         batch_size, max_seq_len, _ = input_tensor.shape
         # Init rnn's states by zeros
         rnn_forward_h = self.make_gpu(torch.zeros(batch_size, self.hidden_dim))
--- main.py
+++ main.py
@@ -68,9 +68,9 @@ if __name__ == "__main__":
 
     args.epoch_num = 200
     #args.lr_decay = 0.05
-    #args.rnn_type = 'GRU'
-    args.checkpoint_fn = 'tagger_model_es_par_GRU_experimental.bin'
-    args.report_fn = 'report_es_par_GRU_experimental.txt'
+    args.rnn_type = 'GRU'
+    args.checkpoint_fn = 'tagger_model_es_par_GRU_old_experimental.bin'
+    args.report_fn = 'report_es_par_GRU_old_experimental.txt'
 
     # Load CoNNL data as sequences of strings of tokens and corresponding tags
     token_sequences_train, tag_sequences_train = read_CoNNL_dat_abs(args.fn_train)
