7bf0ce12346b2e10ae8c463a71244671e42f61f5
artem <a.chernodub@gmail.com>
2018-07-31 20:44:18 +0300

    Add determined method to make batches

--- classes/datasets_bank.py
+++ classes/datasets_bank.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 class DatasetsBank():
@@ -36,10 +37,22 @@ class DatasetsBank():
         self.inputs_tensor_test = self.sequences_indexer.idx2tensor(self.inputs_idx_test)
         self.targets_tensor_test = self.sequences_indexer.idx2tensor(self.targets_idx_test)
 
-    def get_train_batch(self, batch_size=0):
-        if batch_size == 0:
-            return self.inputs_tensor_train, self.targets_tensor_train
-        batch_indices = random.sample(range(0, self.train_data_num), batch_size)
+    def get_train_batch_tensor(self, batch_indices):
+        batch_size = len(batch_indices)
         inputs_tensor_train_batch = self.inputs_tensor_train[batch_indices, :].view(batch_size, self.train_max_seq_len)
         targets_tensor_train_batch = self.targets_tensor_train[batch_indices, :].view(batch_size, self.train_max_seq_len)
         return inputs_tensor_train_batch, targets_tensor_train_batch
+
+    def shuffle_train_batches(self, batch_size=10):
+        if batch_size == 0:
+            return [self.inputs_tensor_train], [self.targets_tensor_train]
+        random_indices = np.random.permutation(np.arange(self.train_data_num))
+        batch_num = self.train_data_num // batch_size
+        inputs_tensor_train_list = []
+        targets_tensor_train_list = []
+        for k in range(batch_num): # we ignore the last batch
+            batch_indices = random_indices[k:k + batch_size].tolist()
+            inputs_tensor_train_batch, targets_tensor_train_batch = self.get_train_batch_tensor(batch_indices)
+            inputs_tensor_train_list.append(inputs_tensor_train_batch)
+            targets_tensor_train_list.append(targets_tensor_train_batch)
+        return inputs_tensor_train_list, targets_tensor_train_list
