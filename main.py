6a8dd11e8656fadb01ba971b36e20993e0bd3a5d
artem <a.chernodub@gmail.com>
2018-08-29 14:16:51 +0300

    Move back NER default settings

--- main.py
+++ main.py
@@ -93,10 +93,11 @@ if __name__ == "__main__":
     args.model = 'BiRNN'
     #args.model = 'BiRNNCNN'
     #args.model = 'BiRNNCNNCRF'
-    args.epoch_num = 100
+    args.epoch_num = 50
     args.rnn_hidden_dim = 100
     args.batch_size = 10
     #args.gpu = -1
+    args.lr = 0.015
     args.lr_decay = 0.05
     args.rnn_type = 'LSTM'
     #args.checkpoint_fn = 'tagger_model_BiRNNCNN_NER_nosb.hdf5'
