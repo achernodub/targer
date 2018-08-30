3212ab0c1fb0cccd5f4fcd1cebb1b0a2d2aebe1d
artem <a.chernodub@gmail.com>
2018-08-30 14:34:12 +0300

    exp2

--- models/tagger_base.py
+++ models/tagger_base.py
@@ -31,8 +31,8 @@ class TaggerBase(nn.Module):
             self.cpu()
 
     def clip_gradients(self, clip_grad):
-        #pass
-        nn.utils.clip_grad_norm_(self.parameters(), clip_grad) ###############################
+        pass
+        #nn.utils.clip_grad_norm_(self.parameters(), clip_grad) ###############################
 
     def save(self, checkpoint_fn):
         self.cpu()
