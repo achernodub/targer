9f23039fc601d649325540cf19e5340d90d52fd1
artem <a.chernodub@gmail.com>
2018-09-17 13:48:48 +0300

    exp1

--- main.py
+++ main.py
@@ -75,7 +75,13 @@ if __name__ == "__main__":
     #args.emb_dim = 300
     #args.rnn_hidden_dim = 300
     #args.epoch_num = 100
-    #args.batch_size = 10 #
+    #args.batch_size = 10
+
+    args.fn_train = 'data/AM/persuasive_essays/Paragraph_Level/train.dat.abs'
+    args.fn_dev = 'data/AM/persuasive_essays/Paragraph_Level/dev.dat.abs'
+    args.fn_test = 'data/AM/persuasive_essays/Paragraph_Level/test.dat.abs'
+    args.batch_size = 1
+    args.epoch_num = 5
 
     np.random.seed(args.seed_num)
     torch.manual_seed(args.seed_num)
