6a943d449411aee9feefd3c06ea722dc5ad6a82f
artem <a.chernodub@gmail.com>
2018-09-18 14:18:58 +0300

    exp4

--- main.py
+++ main.py
@@ -85,6 +85,11 @@ if __name__ == "__main__":
     #args.batch_size = 1
     #args.model = 'BiRNNCNNCRF'
 
+    args.lr = 0.015
+    args.lr_decay = 0.05
+    args.epoch_num = 200
+    args.batch_size = 10
+
     np.random.seed(args.seed_num)
     torch.manual_seed(args.seed_num)
     if args.gpu >= 0:
