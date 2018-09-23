08ef6fc0b261ca1c98c2cae67be772b0c2d4a182
artem <a.chernodub@gmail.com>
2018-09-23 20:17:29 +0300

    Add couple of recent including sorting

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import LambdaLR
 
 from classes.data_io import DataIO
-<<<<<<< HEAD
 from classes.datasets_bank import DatasetsBank, DatasetsBankSorted
-=======
-from classes.datasets_bank import DatasetsBankSorted
->>>>>>> 99a3ed7d8b47c43898014e1a24a82eebbf40826f
 
 from seq_indexers.seq_indexer_word import SeqIndexerWord
 from seq_indexers.seq_indexer_tag import SeqIndexerTag
@@ -59,10 +55,10 @@ if __name__ == "__main__":
     parser.add_argument('--dropout_ratio', type=float, default=0.5, help='Dropout ratio.')
     parser.add_argument('--clip_grad', type=float, default=5, help='Clipping gradients maximum L2 norm.')
     parser.add_argument('--opt_method', default='sgd', help='Optimization method: "sgd", "adam".')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
+    parser.add_argument('--lr', type=float, default=0.005, help='Learning rate.')
     parser.add_argument('--lr_decay', type=float, default=0, help='Learning decay rate.') # 0.05
     parser.add_argument('--momentum', type=float, default=0.9, help='Learning momentum rate.')
-    parser.add_argument('--batch_size', type=int, default=10, help='Batch size, samples.')
+    parser.add_argument('--batch_size', type=int, default=1, help='Batch size, samples.')
     parser.add_argument('--verbose', type=bool, default=True, help='Show additional information.')
     parser.add_argument('--seed_num', type=int, default=42, help='Random seed number, but 42 is the best forever!')
     parser.add_argument('--checkpoint_fn', default=None, help='Path to save the trained model.')
@@ -74,11 +70,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-<<<<<<< HEAD
     # Non-default settings
-=======
-    #Custom params
->>>>>>> 99a3ed7d8b47c43898014e1a24a82eebbf40826f
     args.word_seq_indexer_path = 'wsi_NER.hdf5'
 
     np.random.seed(args.seed_num)
@@ -171,16 +163,12 @@ if __name__ == "__main__":
     if hasattr(tagger, 'crf_layer'):
         tagger.crf_layer.init_transition_matrix_empirical(tag_sequences_train)
 
-<<<<<<< HEAD
     if args.opt_method == 'sgd':
         optimizer = optim.SGD(list(tagger.parameters()), lr=args.lr, momentum=args.momentum)
     elif args.opt_method == 'adam':
         optimizer = optim.Adam(list(tagger.parameters()), lr=args.lr, betas=(0.9, 0.999))
     else:
         raise ValueError('Unknown tagger model, must be one of "sgd"/"adam".')
-=======
-    optimizer = optim.SGD(list(tagger.parameters()), lr=args.lr, momentum=args.momentum, weight_decay=0.01)
->>>>>>> 99a3ed7d8b47c43898014e1a24a82eebbf40826f
     scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1/(1 + args.lr_decay*epoch))
     iterations_num = int(datasets_bank.train_data_num / args.batch_size)
     best_f1_dev = -1
@@ -199,11 +187,7 @@ if __name__ == "__main__":
             tagger.zero_grad()
             loss = tagger.get_loss(word_sequences_train_batch, tag_sequences_train_batch)
             loss.backward()
-<<<<<<< HEAD
             nn.utils.clip_grad_value_(tagger.parameters(), args.clip_grad)
-=======
-            nn.utils.clip_grad_norm_(tagger.parameters(), args.clip_grad)
->>>>>>> 99a3ed7d8b47c43898014e1a24a82eebbf40826f
             optimizer.step()
             loss_sum += loss.item()
             if i % 1 == 0:
