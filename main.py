f3bab5750beac87da48b25d177932e60afea560b
artem <a.chernodub@gmail.com>
2018-09-24 14:34:38 +0300

    Sorting & other

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     # Custom params
     #args.checkpoint_fn = 'tagger_NER.hdf5'
-    args.word_seq_indexer_path = 'wsi_NER.hdf5'
+    #args.word_seq_indexer_path = 'wsi_NER.hdf5'
 
     np.random.seed(args.seed_num)
     torch.manual_seed(args.seed_num)
