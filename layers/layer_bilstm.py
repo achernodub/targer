4f4d7a6c612ca683b289889e5397d412e01dccc3
artem <a.chernodub@gmail.com>
2018-09-26 13:52:16 +0300

    fix in lstm_layer

--- layers/layer_bilstm.py
+++ layers/layer_bilstm.py
@@ -20,29 +20,31 @@ class LayerBiLSTM(LayerBiRNNBase):
                       num_layers=1,
                       batch_first=True,
                       bidirectional=True)
-        nn.init.xavier_uniform_(rnn.weight_hh_l0)
-        nn.init.xavier_uniform_(rnn.weight_hh_l0_reverse)
-        nn.init.xavier_uniform_(rnn.weight_ih_l0)
-        nn.init.xavier_uniform_(rnn.weight_ih_l0_reverse)
-        rnn.bias_hh_l0.data.fill_(0)
-        rnn.bias_hh_l0_reverse.data.fill_(0)
-        rnn.bias_ih_l0.data.fill_(0)
-        rnn.bias_ih_l0_reverse.data.fill_(0)
+        self.rnn = rnn
+
+    def lstm_custom_init(self):
+        nn.init.xavier_uniform_(self.rnn.weight_hh_l0)
+        nn.init.xavier_uniform_(self.rnn.weight_hh_l0_reverse)
+        nn.init.xavier_uniform_(self.rnn.weight_ih_l0)
+        nn.init.xavier_uniform_(self.rnn.weight_ih_l0_reverse)
+        self.rnn.bias_hh_l0.data.fill_(0)
+        self.rnn.bias_hh_l0_reverse.data.fill_(0)
+        self.rnn.bias_ih_l0.data.fill_(0)
+        self.rnn.bias_ih_l0_reverse.data.fill_(0)
         # Init forget gates to 1
-        for names in rnn._all_weights:
+        for names in self.rnn._all_weights:
             for name in filter(lambda n: 'bias' in n, names):
-                bias = getattr(rnn, name)
+                bias = getattr(self.rnn, name)
                 n = bias.size(0)
                 start, end = n // 4, n // 2
                 bias.data[start:end].fill_(1.)
-        self.rnn = rnn
 
     def forward(self, input_tensor, mask_tensor): #input_tensor shape: batch_size x max_seq_len x dim
         batch_size, max_seq_len, _ = input_tensor.shape
         h0 = self.tensor_ensure_gpu(torch.zeros(self.num_layers * self.num_directions, batch_size, self.hidden_dim))
         c0 = self.tensor_ensure_gpu(torch.zeros(self.num_layers * self.num_directions, batch_size, self.hidden_dim))
         output, _ = self.rnn(input_tensor, (h0, c0))
-        return self.apply_mask(output)  # shape: batch_size x max_seq_len x hidden_dim*2
+        return self.apply_mask(output, mask_tensor)  # shape: batch_size x max_seq_len x hidden_dim*2
 
     def is_cuda(self):
         return self.rnn.weight_hh_l0.is_cuda
