971ddb5d75a29b158d96a2f3c28b07a08afa3ca7
artem <a.chernodub@gmail.com>
2018-09-27 17:20:33 +0300

    bugfix

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     parser.add_argument('--check_for_lowercase', type=bool, default=True, help='Read characters caseless.')
     parser.add_argument('--epoch_num', type=int, default=200, help='Number of epochs.')
     parser.add_argument('--min_epoch_num', type=int, default=50, help='Minimum number of epochs.')
-    parser.add_argument('--rnn_hidden_dim', type=int, default=100, help='Number hidden units in the recurrent layer.')
+    parser.add_argument('--rnn_hidden_dim', type=int, default=200, help='Number hidden units in the recurrent layer.')
     parser.add_argument('--rnn_type', default='LSTM', help='RNN cell units type: "Vanilla", "LSTM", "GRU".')
     parser.add_argument('--char_embeddings_dim', type=int, default=25, help='Char embeddings dim, only for char CNNs.')
     parser.add_argument('--word_len', type=int, default=20, help='Max length of words in characters for char CNNs.')
@@ -65,12 +65,13 @@ if __name__ == "__main__":
     parser.add_argument('--save_best', type=bool, default=True, help = 'Save best on dev model as a final.')
 
     args = parser.parse_args()
+    args.save_checkpoint_fn = '%s_%s_tagger.hdf5' % (get_datetime_str(), args.model)
     #args.word_seq_indexer_path = 'wsi_NER.hdf5'
-    args.model = 'BiRNN'
+    #args.model = 'BiRNN'
     #args.emb_fn = 'embeddings/glove.6B.100d.txt'
     #args.emb_dim = 100
-    args.emb_fn = 'embeddings/wiki.en.vec'
-    args.emb_dim = 300
+    #args.emb_fn = 'embeddings/wiki.en.vec'
+    #args.emb_dim = 300
     #args.emb_fn = 'embeddings/GoogleNews-vectors-negative300.txt'
     #args.emb_dim = 300
 
@@ -123,7 +124,7 @@ if __name__ == "__main__":
     scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1/(1 + args.lr_decay*epoch))
 
     # Prepare report and temporary variables for "save best" strategy
-    report_fn = 'report_%s_%s_batch%d_%dep.txt' % (get_datetime_str(), args.model, args.batch_size, args.epoch_num)
+    report_fn = '%s_%s_report.txt' % (get_datetime_str(), args.model)
     report = Report(report_fn, args, score_names=('train loss', 'f1-train', 'f1-dev', 'f1-test', 'acc. train', 'acc. dev',
                                                   'acc. test'))
     iterations_num = int(datasets_bank.train_data_num / args.batch_size)
