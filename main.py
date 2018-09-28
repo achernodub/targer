73508dec3c75e94bf45585f8de7bb6e878bd5180
artem <a.chernodub@gmail.com>
2018-09-28 08:15:16 +0300

    exp31

--- main.py
+++ main.py
@@ -66,11 +66,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     args.save_checkpoint_fn = '%s_%s_tagger.hdf5' % (get_datetime_str(), args.model)
-    args.word_seq_indexer_path = 'wsi_NER.hdf5'
-    #args.model = 'BiRNN'
-    args.rnn_hidden_dim = 200
-    #args.emb_fn = 'embeddings/glove.6B.100d.txt'
-    #args.emb_dim = 100
+    #args.word_seq_indexer_path = 'wsi_NER.hdf5'
+    args.model = 'BiRNNCRF'
+    #args.rnn_hidden_dim = 200
+    args.emb_fn = 'embeddings/glove.6B.100d.txt'
+    args.emb_dim = 100
     #args.emb_fn = 'embeddings/wiki.en.vec'
     #args.emb_dim = 300
     #args.emb_fn = 'embeddings/GoogleNews-vectors-negative300.txt'
