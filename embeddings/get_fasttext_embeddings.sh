2ee2e781b539b6f1d491698c17b3341ba0e15d15
artem <a.chernodub@gmail.com>
2018-11-05 15:19:09 +0200

    curr

--- embeddings/get_fasttext_embeddings.sh
+++ embeddings/get_fasttext_embeddings.sh
@@ -1,3 +1,4 @@
-wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.vec.zip
-#unzip glove.6B.100d.zip
-#rm glove.6B.100d.zip
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip
+#wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.vec.zip
+unzip wiki.en.zip
+rm wiki.en.zip
