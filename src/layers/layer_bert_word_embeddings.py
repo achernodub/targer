84d8bef7615860c3250a6e459a9d41c940dd5b53
Artem Chernodub <a.chernodub@gmail.com>
2019-01-28 08:23:15 +0200

    Add BERT, debug

--- src/layers/layer_bert_word_embeddings.py
+++ src/layers/layer_bert_word_embeddings.py
@@ -7,11 +7,12 @@ from pytorch_pretrained_bert import BertModel
 
 class LayerBertWordEmbeddings(LayerBase):
     """LayerWordEmbeddings implements word embeddings."""
-    def __init__(self, word_seq_indexer, gpu, output_dim=300):
+    def __init__(self, word_seq_indexer, gpu, output_dim=300, output_bert_num=2):
         super(LayerBertWordEmbeddings, self).__init__(gpu)
         self.word_seq_indexer = word_seq_indexer
         self.gpu = gpu
-        self.bert_dim = 768 * 4
+        self.output_bert_num = output_bert_num
+        self.bert_dim = 768 * output_bert_num
         self.output_dim = output_dim
         self.lin_layer = nn.Linear(in_features=self.bert_dim, out_features=output_dim)
         self.bert_model = BertModel.from_pretrained('bert-base-cased')
@@ -24,6 +25,15 @@ class LayerBertWordEmbeddings(LayerBase):
         segments_tensor = self.tensor_ensure_gpu(torch.zeros(tokens_tensor.shape, dtype=torch.long))
         self.bert_model.eval()
         y, _ = self.bert_model(tokens_tensor, segments_tensor)  # y : batch_size x max_seq_len x dim
-        bert_features = torch.cat((y[8], y[9], y[10], y[11]), dim=2) # 2 x 7 x 3072
+        if self.output_bert_num == 4:
+            bert_features = torch.cat((y[8], y[9], y[10], y[11]), dim=2) # 2 x 7 x 3072
+        elif self.output_bert_num == 3:
+            bert_features = torch.cat((y[9], y[10], y[11]), dim=2) # 2 x 7 x 3072
+        elif self.output_bert_num == 2:
+            bert_features = torch.cat((y[10], y[11]), dim=2) # 2 x 7 x 3072
+        elif self.output_bert_num == 1:
+            bert_features = torch.cat((y[11]), dim=2) # 2 x 7 x 3072
+        else:
+            raise NotImplementedError()
         compressed_bert_features = self.lin_layer(bert_features) # 2 x 7 x 300
         return compressed_bert_features
